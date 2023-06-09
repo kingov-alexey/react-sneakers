ab587d87d5e4836846b8e12ac239aac9c96f1317
korolev <korolev@toowto.kz>
2023-06-09 18:10:07 +0600

    lesson 3/26:00

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import Card from './components/Card/Card';
-import Header from './components/Header/Header';
-import Drawer from './components/Drawer/Drawer'
+import Card from "./components/Card/Card";
+import Header from "./components/Header/Header";
+import Drawer from "./components/Drawer/Drawer";
 
+const arr = [
+  {
+    name: "Мужские Кроссовки Nike  Blazer Mid Suede",
+    price: 12999,
+  },
+  {
+    name: "Мужские Кроссовки Nike  Air Max 270",
+    price: 15600,
+  },
+];
 
 function App() {
   return (
     <div className="wrapper clear">
-      
-      
+      <Drawer />
 
-<Drawer />
-
-     
-
-<Header />
+      <Header />
 
       <div className="content p-40">
-
         <div className="d-flex align-center mb-40 justify-between">
           <h1>Все кроссовки</h1>
           <div className="search-block d-flex">
@@ -26,14 +30,10 @@ function App() {
         </div>
 
         <div className="d-flex">
-
-        <Card />
-            <Card />
-            <Card />
-            <Card />
-
-
-
+          <Card />
+          <Card />
+          <Card />
+          <Card />
         </div>
       </div>
     </div>
