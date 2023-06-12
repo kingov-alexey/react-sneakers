b7a93219f87c36fe1768a93c7b64e933092cf256
kingov.alexey <kingov_alexey@mail.ru>
2023-06-13 00:48:43 +0600

    refactor 13.06.2023

--- src/App.js
+++ src/App.js
@@ -1,48 +1,49 @@
-import Card from "./components/Card/Card";
-import Header from "./components/Header/Header";
-import Drawer from "./components/Drawer/Drawer";
+import Card from './components/Card/Card';
+import Header from './components/Header/Header';
+import Drawer from './components/Drawer/Drawer';
 
-const callback = ()=>{
+const callback = () => {
   alert(1114);
-}
+};
 
 const arr = [
   {
     imageUrl: '/img/sneakers/1.jpg',
-    name: "Мужские Кроссовки Nike  Blazer Mid Suede",
+    name: 'Мужские Кроссовки Nike  Blazer Mid Suede',
     price: 12999,
   },
   {
     imageUrl: '/img/sneakers/2.jpg',
-    name: "Мужские Кроссовки Nike  Air Max 270",
+    name: 'Мужские Кроссовки Nike  Air Max 270',
     price: 15600,
+  },
+  {
+    imageUrl: '/img/sneakers/3.jpg',
+    name: 'Наименование кросовок 3',
+    price: 33333333,
   },
 ];
 
-
-
 function App() {
   return (
-    <div className="wrapper clear">
+    <div className='wrapper clear'>
       <Drawer />
 
       <Header />
 
-      <div className="content p-40">
-        <div className="d-flex align-center mb-40 justify-between">
+      <div className='content p-40'>
+        <div className='d-flex align-center mb-40 justify-between'>
           <h1>Все кроссовки</h1>
-          <div className="search-block d-flex">
-            <img src="/img/search.svg" alt="Search" />
-            <input type="text" placeholder="Поиск..." />
+          <div className='search-block d-flex'>
+            <img src='/img/search.svg' alt='Search' />
+            <input type='text' placeholder='Поиск...' />
           </div>
         </div>
 
-        <div className="d-flex">
-
-          {
-            arr.map(obj => <Card imageUrl={obj.imageUrl} name={obj.name} price={obj.price} callback={callback}/>)
-          }
-
+        <div className='d-flex'>
+          {arr.map(obj => (
+            <Card imageUrl={obj.imageUrl} name={obj.name} price={obj.price} callback={callback} />
+          ))}
         </div>
       </div>
     </div>
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,30 +1,24 @@
 import styles from './Card.module.scss';
 
-function Card(props){
-    return (
-      <div className={styles.card}>
+function Card(props) {
+  return (
+    <div className={styles.card}>
       <div className={styles.favorite}>
-        <img src="/img/heart-liked.svg" alt="Unliked" />
+        <img src='/img/heart-liked.svg' alt='Unliked' />
       </div>
-      <img
-        width={133}
-        height={112}
-        src={props.imageUrl}
-        alt="Sneakers"
-      />
+      <img width={133} height={112} src={props.imageUrl} alt='Sneakers' />
       <h5>{props.name}</h5>
-      <div className="d-flex justify-between align-center">
-        <div className="d-flex flex-column">
+      <div className='d-flex justify-between align-center'>
+        <div className='d-flex flex-column'>
           <span>Цена:</span>
           <b>{props.price} руб.</b>
         </div>
         <button className={styles.button} onClick={props.callback}>
-          <img width={11} height={11} src="/img/plus.svg" alt="plus" />
+          <img width={11} height={11} src='/img/plus.svg' alt='plus' />
         </button>
       </div>
     </div>
-
-    );
+  );
 }
 
 export default Card;
