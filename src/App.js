c4a04e606d6a4fee877007808813db452607dab6
kingov.alexey <kingov_alexey@mail.ru>
2023-06-23 01:12:04 +0600

    home commit test

--- src/App.js
+++ src/App.js
@@ -1,58 +1,52 @@
-import React from "react";
+import React from 'react';
 
-import Header from "./components/Header/Header";
-import Drawer from "./components/Drawer/Drawer";
-import axios from "axios";
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Favorites from "./pages/Favorites/Favorites";
+import Header from './components/Header/Header';
+import Drawer from './components/Drawer/Drawer';
+import axios from 'axios';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Favorites from './pages/Favorites/Favorites';
 
 function App() {
-  const pathBackendApi = "http://localhost:9999"; //путь к api
+  const pathBackendApi = 'http://localhost:9999'; //путь к api
   const [items, setItems] = React.useState([]); //Кросовки на складе
   const [cartItems, setCartItems] = React.useState([]); //кросовки в корзине
   const [favorites, setFavorites] = React.useState([]); //желаемые кросовки
   const [cartOpened, setCarOpened] = React.useState(false); //тогл боковой панели
-  const [searchValue, setSearchValue] = React.useState(""); //строка поиска
+  const [searchValue, setSearchValue] = React.useState(''); //строка поиска
   const [isLoading, setIsLoading] = React.useState(true);
 
   //* ****** Начало ЭНДПОИНТОВ */
 
   // * ********* table-CART CRUD *************************
   //CREATE ELEMENT IN CART
-  const onAddToCart = (obj) => {
+  const onAddToCart = obj => {
     console.log(obj);
-try {
-  if (cartItems.find((item)=> Number(item.id) === Number(obj.id))){
-    axios.delete(`${pathBackendApi}/table-cart/${obj.id}`);
-    setCartItems(prev => prev.filter(item => Number(item.id) !== Number(obj.id)));
-
-  } else {
-     
-  axios.post(`${pathBackendApi}/table-cart`, obj);
-  setCartItems((prev) => [...prev, obj]);
-
-  }
-
-  
-} catch (error) {
-  console.log('не удалось добавть товар в корзину', error);
-  
-}
+    try {
+      if (cartItems.find(item => Number(item.id) === Number(obj.id))) {
+        axios.delete(`${pathBackendApi}/table-cart/${obj.id}`);
+        setCartItems(prev => prev.filter(item => Number(item.id) !== Number(obj.id)));
+      } else {
+        axios.post(`${pathBackendApi}/table-cart`, obj);
+        setCartItems(prev => [...prev, obj]);
+      }
+    } catch (error) {
+      console.log('не удалось добавть товар в корзину', error);
+    }
   };
 
   //READ ELEMENT FROM CART
   // const onReadTableCart = () => {
-    
+
   // };
 
   //UPDATE ELEMENT IN CART
   //..
 
   // DELETE ELEMENT FROM CART
-  const onRemoveFromCart = (id) => {
+  const onRemoveFromCart = id => {
     axios.delete(`${pathBackendApi}/table-cart/${id}`);
-    setCartItems((prev) => prev.filter((item) => item.id !== id));
+    setCartItems(prev => prev.filter(item => item.id !== id));
   };
 
   // ********** table-sneakers CRUD *************************
@@ -66,21 +60,18 @@ try {
   // ********** table-favorites CRUD *************************
   // CREATE table-favotites
 
-  const onAddToFavorite = async (obj) => {
+  const onAddToFavorite = async obj => {
     try {
-      if (favorites.find((favObj) => favObj.id === obj.id)) {
+      if (favorites.find(favObj => favObj.id === obj.id)) {
         axios.delete(`${pathBackendApi}/table-favorites/${obj.id}`);
         // setFavorites((prev) => prev.filter((item) => item.id !== obj.id));
       } else {
-        const { data } = await axios.post(
-          `${pathBackendApi}/table-favorites`,
-          obj
-        );
-        setFavorites((prev) => [...prev, data]);
+        const { data } = await axios.post(`${pathBackendApi}/table-favorites`, obj);
+        setFavorites(prev => [...prev, data]);
       }
     } catch (error) {
-      alert("не удалось добавить в фавориты");
-      console.log("не удалось добавить в фавориты", error);
+      alert('не удалось добавить в фавориты');
+      console.log('не удалось добавить в фавориты', error);
     }
   };
 
@@ -100,20 +91,18 @@ try {
 
   //clearInputSearch очистка поля поиска
   let clearInputSearch = () => {
-    setSearchValue("");
+    setSearchValue('');
   };
 
   //При изменении фиксировать строку поиска
-  const onChangeSearchInput = (event) => {
+  const onChangeSearchInput = event => {
     console.log(event.target.value);
     setSearchValue(event.target.value);
   };
 
   //хук юз эффект...
   React.useEffect(() => {
-
-
-    async function fetchData(){
+    async function fetchData() {
       setIsLoading(true);
       const cartResponse = await axios.get(`${pathBackendApi}/table-cart`);
       const favoriteResponse = await axios.get(`${pathBackendApi}/table-favorites`);
@@ -126,27 +115,22 @@ try {
     }
 
     fetchData();
-
   }, []);
 
   return (
-    <div className="wrapper clear">
+    <div className='wrapper clear'>
       {/* можно так */}
       {/* {cartOpened ? <Drawer onClickCart={onClickCart}/> : null} */}
       {/* а можно верез && */}
       {cartOpened && (
-        <Drawer
-          items={cartItems}
-          onClickCart={onClickCart}
-          onRemoveFromCart={onRemoveFromCart}
-        />
+        <Drawer items={cartItems} onClickCart={onClickCart} onRemoveFromCart={onRemoveFromCart} />
       )}
 
       <Header onClickCart={onClickCart} />
 
       <Routes>
         <Route
-          path="/"
+          path='/'
           element={
             <Home
               items={items}
@@ -162,10 +146,8 @@ try {
           }
         />
         <Route
-          path="/favorites"
-          element={
-            <Favorites items={favorites} onAddToFavorite={onAddToFavorite} />
-          }
+          path='/favorites'
+          element={<Favorites items={favorites} onAddToFavorite={onAddToFavorite} />}
         />
       </Routes>
     </div>
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import ContentLoader from "react-content-loader"
-import styles from "./Card.module.scss";
+import React, { useState } from 'react';
+import ContentLoader from 'react-content-loader';
+import styles from './Card.module.scss';
 
 function Card({
   id,
@@ -12,7 +12,7 @@ function Card({
   onPlus,
   favorited = false,
   added = false,
-  loading = false
+  loading = false,
 }) {
   const [isAdded, setIsAdded] = useState(added);
   const [isFavorite, setIsFavorite] = useState(favorited);
@@ -28,61 +28,62 @@ function Card({
   };
 
   const onClickCard = () => {
-    alert("onClickCard показать модалку");
+    alert('onClickCard показать модалку');
   };
 
   React.useEffect(() => {}, [isAdded]);
 
   return (
     <div className={styles.card}>
-      {
-        loading ?  <ContentLoader 
-        speed={3}
-        width={156}
-        height={275}
-        viewBox="0 0 156 275"
-        backgroundColor="#ccadfe"
-        foregroundColor="#abeeff"
-      >
-        <rect x="0" y="0" rx="9" ry="9" width="32" height="32" /> 
-        <rect x="0" y="50" rx="25" ry="25" width="156" height="112" /> 
-        <rect x="0" y="182" rx="9" ry="9" width="156" height="15" /> 
-        <rect x="0" y="203" rx="9" ry="9" width="119" height="15" /> 
-        <rect x="2" y="234" rx="7" ry="7" width="62" height="13" /> 
-        <rect x="0" y="252" rx="7" ry="7" width="96" height="13" /> 
-        <rect x="124" y="234" rx="9" ry="9" width="32" height="32" />
-      </ContentLoader> : <>
-      <div
-        className={styles.favorite}
-        onClick={() => {
-          onClickFavorite({ imageUrl, title, price });
-        }}
-      >
-        <img
-          src={isFavorite ? "/img/liked.svg" : "/img/unliked.svg"}
-          alt={isFavorite ? "liked" : "Unliked"}
-        />
-      </div>
+      {loading ? (
+        <ContentLoader
+          speed={3}
+          width={156}
+          height={275}
+          viewBox='0 0 156 275'
+          backgroundColor='#cccccc'
+          foregroundColor='#ffffff'
+        >
+          <rect x='0' y='0' rx='9' ry='9' width='32' height='32' />
+          <rect x='0' y='50' rx='25' ry='25' width='156' height='112' />
+          <rect x='0' y='182' rx='9' ry='9' width='156' height='15' />
+          <rect x='0' y='203' rx='9' ry='9' width='119' height='15' />
+          <rect x='2' y='234' rx='7' ry='7' width='62' height='13' />
+          <rect x='0' y='252' rx='7' ry='7' width='96' height='13' />
+          <rect x='124' y='234' rx='9' ry='9' width='32' height='32' />
+        </ContentLoader>
+      ) : (
+        <>
+          <div
+            className={styles.favorite}
+            onClick={() => {
+              onClickFavorite({ id, imageUrl, title, price });
+            }}
+          >
+            <img
+              src={isFavorite ? '/img/liked.svg' : '/img/unliked.svg'}
+              alt={isFavorite ? 'liked' : 'Unliked'}
+            />
+          </div>
 
-      <div className={styles.slider} onClick={onClickCard}>
-        <img width={133} height={112} src={imageUrl} alt="Sneakers" />
-      </div>
-      <h5>{title}</h5>
-      <div className="d-flex justify-between align-center">
-        <div className="d-flex flex-column">
-          <span>Цена:</span>
-          <b>{price} руб.</b>
-        </div>
-        <img
-          className={styles.plus}
-          src={isAdded ? "/img/btn-checked.svg" : "/img/btn-plus.svg"}
-          alt="plus"
-          onClick={onClickPlus}
-        />
-      </div>
-      </>
-      }
-    
+          <div className={styles.slider} onClick={onClickCard}>
+            <img width={133} height={112} src={imageUrl} alt='Sneakers' />
+          </div>
+          <h5>{title}</h5>
+          <div className='d-flex justify-between align-center'>
+            <div className='d-flex flex-column'>
+              <span>Цена:</span>
+              <b>{price} руб.</b>
+            </div>
+            <img
+              className={styles.plus}
+              src={isAdded ? '/img/btn-checked.svg' : '/img/btn-plus.svg'}
+              alt='plus'
+              onClick={onClickPlus}
+            />
+          </div>
+        </>
+      )}
     </div>
   );
 }
